1dcd6d9183747dddfcd999351146ccfda103a517
see <upcwangying@126.com>
2022-06-12 18:46:38 +0800

    build: bugfixes

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,18 @@
 // GlobalComponents for Volar
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-
+    BvBadge: typeof import('bootstrap-vue-plus')['BvBadge']
+    BvButton: typeof import('bootstrap-vue-plus')['BvButton']
+    BvButtonGroup: typeof import('bootstrap-vue-plus')['BvButtonGroup']
+    BvConfigProvider: typeof import('bootstrap-vue-plus')['BvConfigProvider']
+    BvEmpty: typeof import('bootstrap-vue-plus')['BvEmpty']
+    BvForm: typeof import('bootstrap-vue-plus')['BvForm']
+    BvFormItem: typeof import('bootstrap-vue-plus')['BvFormItem']
+    BvIcon: typeof import('bootstrap-vue-plus')['BvIcon']
   }
 
   interface ComponentCustomProperties {
-
+    $message: typeof import('bootstrap-vue-plus')['BvMessage']
   }
 }
 
--- typings/components.d.ts
+++ typings/components.d.ts
@@ -4,11 +4,18 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   // GlobalComponents for Volar
   export interface GlobalComponents {
-
+    BvBadge: typeof import('../packages/bootstrap-vue-plus')['BvBadge']
+    BvButton: typeof import('../packages/bootstrap-vue-plus')['BvButton']
+    BvButtonGroup: typeof import('../packages/bootstrap-vue-plus')['BvButtonGroup']
+    BvConfigProvider: typeof import('../packages/bootstrap-vue-plus')['BvConfigProvider']
+    BvEmpty: typeof import('../packages/bootstrap-vue-plus')['BvEmpty']
+    BvForm: typeof import('../packages/bootstrap-vue-plus')['BvForm']
+    BvFormItem: typeof import('../packages/bootstrap-vue-plus')['BvFormItem']
+    BvIcon: typeof import('../packages/bootstrap-vue-plus')['BvIcon']
   }
 
   interface ComponentCustomProperties {
-
+    $message: typeof import('../packages/bootstrap-vue-plus')['BvMessage']
   }
 }
 
